02f4583b9138ec4fc34f60f0e2a69eaa4d73e96d
JuandaMonkey <juandanieldecide@gmail.com>
2026-06-25 21:54:42 -0700

    agregación de rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+// layout
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () =>
+                    import('./pages/admin/dashboard-component/dashboard-component').then((m) => m.DashboardComponent)
+            },
+            {
+                path: 'alumnos',
+                loadComponent: () =>
+                    import('./pages/admin/alumnos-component/alumnos-component').then((m) => m.AlumnosComponent),
+                children: [
+                    {
+                        path: 'agregar-alumno',
+                        loadComponent: () =>
+                            import('./shared/components/modal-agregar-alumno/modal-agregar-alumno').then((m) => m.ModalAgregarAlumno)
+                    }
+                ]
+            },
+            {
+                path: 'calificaciones',
+                loadComponent: () =>
+                    import('./pages/calificaciones-component/calificaciones-component').then((m) => m.CalificacionesComponent),
+                children: [
+                    {
+                        path: 'boleta',
+                        loadComponent: () =>
+                            import('./shared/components/modal-boleta/modal-boleta').then((m) => m.ModalBoleta)
+                    }
+                ]
+            }
+        ]
+    }
+];
