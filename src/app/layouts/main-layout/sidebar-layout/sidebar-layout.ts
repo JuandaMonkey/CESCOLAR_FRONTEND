ea93e82a619fa9c65fb69ad48a8181aa91a8143a
JuandaMonkey <juandanieldecide@gmail.com>
2026-06-25 22:23:13 -0700

    estructura basica sidebar html

--- src/app/layouts/main-layout/sidebar-layout/sidebar-layout.ts
+++ src/app/layouts/main-layout/sidebar-layout/sidebar-layout.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive } from '@angular/router';
+// sidebar
+import { sideBarData } from '../../../core/data/sidebar.data';
+import { SideBarModel, SideBarItem } from '../../../core/models/sidebar.model';
 
 @Component({
   selector: 'app-sidebar-layout',
-  imports: [],
+  standalone: true,
+  imports: [ 
+    RouterLink, 
+    RouterLinkActive],
   templateUrl: './sidebar-layout.html',
   styleUrl: './sidebar-layout.css',
 })
 export class SidebarLayout {
+  nombreUsuario = 'Lucía Reyes';
+  rolUsuario = 'administrador';
 
+  sideBar: SideBarModel = sideBarData;
+
+  manejarClick(item: SideBarItem): void {
+    if (item.onClick === 'logout') {
+      this.cerrarSesion();
+    }
+  }
+  
+  private cerrarSesion(): void {
+    console.log('Cerrar sesión (pendiente de conectar)');
+  } 
 }
